f88342eb0aaea63bf6cad76b5c5169b132a87585
angeladrianmartinezbernardino <angeladrianmartinezbernardino@gmail.com>
2023-12-11 19:27:55 -0600

    Este esqueleto sí funciona.

--- src/main/java/com/example/tap_intellij/Vistas/Lotería.java
+++ src/main/java/com/example/tap_intellij/Vistas/Lotería.java
@@ -3,20 +3,10 @@ package com.example.tap_intellij.Vistas;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /*
  * Ángel Adrián Martínez Bernardino.
@@ -30,18 +20,11 @@ public class Lotería extends Stage {
     private HBox Seleccionar_Tabla;
     private VBox Cambio_de_tablas;
     private VBox Partida;
-    private ImageView Mazo, IV;
-    private Image Dorso, I_Carta;
-    private Button Anterior, Siguiente, Iniciar, Reiniciar, Botón;
+    private Button Anterior, Siguiente, Iniciar, Reiniciar;
     private GridPane Tabla;
-    private String[] Carta;
-    private String Carta_actual;
-    private int i, j, Bucle_para_tablas, Posición;
     private int Índice_de_la_tabla_actual = 0;
-    private List<String> Cartas_mezcladas;
 
     public Lotería() {
-        this.Crear_mazo();
         this.inicializarTablas();
         this.Crear_UI();
         this.Escena = new Scene(this.Juego, 600.0, 625.0);
@@ -62,7 +45,7 @@ public class Lotería extends Stage {
         this.Cambio_de_tablas = new VBox(this.Tabla, this.Seleccionar_Tabla);
         this.Cambio_de_tablas.setSpacing(20.0);
         this.Iniciar = new Button("Iniciar");
-        this.Partida = new VBox(this.Mazo, this.Iniciar);
+        this.Partida = new VBox(this.Iniciar);
         this.Juego = new HBox(this.Cambio_de_tablas, this.Partida);
         this.Juego.setPadding(new Insets(20.0));
         this.Reiniciar = new Button("Comenzar de nuevo");
@@ -70,34 +53,14 @@ public class Lotería extends Stage {
         this.Partida.getChildren().add(this.Reiniciar);
     }
 
-    private void Crear_mazo() {
-        Carta = new String[]{"1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg", "31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", "36.jpg", "37.jpg", "38.jpg", "39.jpg", "40.jpg", "41.jpg", "42.jpg", "43.jpg", "44.jpg", "45.jpg", "46.jpg", "47.jpg", "48.jpg", "49.jpg", "50.jpg", "51.jpg", "52.jpg", "53.jpg", "54.jpg"};
-        Dorso = new Image((new File("C:\\Users\\AAdri\\OneDrive\\Multimedia\\Documentos\\Programas\\IntelliJ\\TAP_IntelliJ\\src\\main\\resources\\Imágenes\\Dorso.png")).toURI().toString());
-        this.Mazo = new ImageView(Dorso);
-        this.Mazo.setFitWidth(119.2);
-        this.Mazo.setFitHeight(168.4);
-    }
-
     private void inicializarTablas() {
-        for (Bucle_para_tablas = 0; Bucle_para_tablas < 5; Bucle_para_tablas++) {
-            Cartas_mezcladas = new ArrayList<>(Arrays.asList(Carta));
-            Collections.shuffle(Cartas_mezcladas);
-            Posición = 0;
-            for (i = 0; i < 4; i++) {
-                for (j = 0; j < 4; j++) {
-                    Carta_actual = Cartas_mezcladas.get(Posición);
-                    Botón = new Button();
-                    try {
-                        I_Carta = new Image(new FileInputStream("C:\\\\Users\\\\AAdri\\\\OneDrive\\\\Multimedia\\\\Documentos\\\\Programas\\\\IntelliJ\\\\TAP_IntelliJ\\\\src\\\\main\\\\resources\\\\Imágenes\\\\" + Carta_actual));
-                        IV = new ImageView(I_Carta);
-                        IV.setFitHeight(126.324);
-                        IV.setFitWidth(89.28);
-                        Botón.setGraphic(IV);
-                    } catch (FileNotFoundException e) {
-                        System.out.println("Archivo no encontrado: " + e.getMessage());
-                    }
-                    Todas_las_tablas[Bucle_para_tablas][i][j] = Botón;
-                    Posición++;
+        for (int numTabla = 0; numTabla < 5; numTabla++) {
+            for (int fila = 0; fila < 4; fila++) {
+                for (int columna = 0; columna < 4; columna++) {
+                    // El texto del botón ahora incluye el índice de la tabla
+                    Button boton = new Button("Botón [" + numTabla + "][" + fila + "][" + columna + "]");
+                    boton.setPrefSize(89.28, 126.324); // Tamaño del botón
+                    Todas_las_tablas[numTabla][fila][columna] = boton;
                 }
             }
         }
@@ -106,9 +69,9 @@ public class Lotería extends Stage {
     private void Crear_tabla(int indiceTabla) {
         this.Tabla = new GridPane();
         Tabla_actual = Todas_las_tablas[indiceTabla];
-        for (i = 0; i < Tabla_actual.length; i++) {
-            for (j = 0; j < Tabla_actual[i].length; j++) {
-                this.Tabla.add(Tabla_actual[i][j], i, j);
+        for (int fila = 0; fila < Tabla_actual.length; fila++) {
+            for (int columna = 0; columna < Tabla_actual[fila].length; columna++) {
+                this.Tabla.add(Tabla_actual[fila][columna], columna, fila);
             }
         }
     }
@@ -121,6 +84,8 @@ public class Lotería extends Stage {
             Índice_de_la_tabla_actual = 0;
         }
         System.out.println("Índice de la tabla actual: " + Índice_de_la_tabla_actual);
-        Crear_tabla(Índice_de_la_tabla_actual);
+        this.Cambio_de_tablas.getChildren().remove(this.Tabla); // Eliminar la tabla anterior
+        Crear_tabla(Índice_de_la_tabla_actual); // Crear la nueva tabla
+        this.Cambio_de_tablas.getChildren().add(0, this.Tabla); // Añadir la nueva tabla en la primera posición del VBox
     }
 }
