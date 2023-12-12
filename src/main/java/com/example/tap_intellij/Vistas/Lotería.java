c426c72a2c4251c42f7ef6fb0f8864ea0cbc7469
angeladrianmartinezbernardino <angeladrianmartinezbernardino@gmail.com>
2023-12-11 22:06:55 -0600

    Más o menos.

--- src/main/java/com/example/tap_intellij/Vistas/Lotería.java
+++ src/main/java/com/example/tap_intellij/Vistas/Lotería.java
@@ -1,17 +1,21 @@
 package com.example.tap_intellij.Vistas;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 /*
  * Ángel Adrián Martínez Bernardino.
@@ -30,15 +34,31 @@ public class Lotería extends Stage {
     private int Índice_de_la_tabla_actual = 0, Tabla, Fila, Columna, Índice, Número_de_carta;
     private List<Integer> Números_disponibles;
 
-    private final Random random = new Random();
+    private Label mazoLabel;
+    private Timeline timeline;
+    private final int tiempoCarta = 4;
+    private List<Integer> cartasJuego;
 
     public Lotería() {
+        this.cartasJuego = new ArrayList<>(Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+                51, 52, 53, 54
+        ));
+        Collections.shuffle(this.cartasJuego);
         this.Iniciar_tablas();
         this.Crear_UI();
         this.Escena = new Scene(this.Juego, 600.0, 625.0);
         this.setTitle("Lotería");
         this.setScene(this.Escena);
         this.show();
+        this.mazoLabel = new Label("Mazo");
+        mazoLabel.setStyle("-fx-background-color: pink; -fx-text-fill: black; -fx-font-size: 20;");
+        mazoLabel.setPrefSize(200, 200);
+        this.Partida.getChildren().add(0, mazoLabel); // Asegúrate de que el mazoLabel sea el primer elemento en Partida
     }
 
     private void Crear_UI() {
@@ -54,10 +74,9 @@ public class Lotería extends Stage {
         this.Cambio_de_tablas.setSpacing(20.0);
         this.Iniciar = new Button("Iniciar");
         this.Iniciar.setOnAction(e -> {
-            // Deshabilita los botones cuando el juego inicia
             Anterior.setDisable(true);
             Siguiente.setDisable(true);
-            iniciarJuego(); // Suponiendo que tienes un método para iniciar el juego
+            iniciarJuego();
         });
         this.Partida = new VBox(this.Iniciar);
         this.Juego = new HBox(this.Cambio_de_tablas, this.Partida);
@@ -110,7 +129,40 @@ public class Lotería extends Stage {
     }
 
     private void iniciarJuego() {
-        // Aquí iría el código para iniciar el juego
-        // Por ejemplo, mezclar las cartas, comenzar a sacar cartas, etc.
+        Anterior.setDisable(true);
+        Siguiente.setDisable(true);
+
+        timeline = new Timeline(new KeyFrame(Duration.seconds(tiempoCarta), e -> sacarCarta()));
+        timeline.setCycleCount(Timeline.INDEFINITE); // Usa INDEFINITE si quieres que continúe hasta que se detenga manualmente
+        timeline.play();
+    }
+
+    private void sacarCarta() {
+        // Generar un número aleatorio entre 1 y 54
+        int cartaActual = cartasJuego.remove(0);
+        mazoLabel.setText("Carta actual es " + cartaActual);
+
+        // Marcar la carta automáticamente después de 4 segundos si el jugador no lo ha hecho
+        new Timeline(new KeyFrame(Duration.seconds(tiempoCarta), event -> {
+            marcarCarta(cartaActual);
+        })).play();
+
+        // Si cartasJuego está vacío, detener el juego
+        if (cartasJuego.isEmpty()) {
+            timeline.stop();
+            mazoLabel.setText("Fin del juego");
+        }
+    }
+
+    private void marcarCarta(int numeroCarta) {
+        // Encuentra el botón en la tabla actual que corresponde al número de carta sacado
+        for (Button[] fila : Tabla_actual) {
+            for (Button boton : fila) {
+                if (boton.getText().endsWith(String.valueOf(numeroCarta))) {
+                    boton.setStyle("-fx-opacity: 0.5;"); // Marcar la carta visualmente
+                    boton.setDisable(true); // Deshabilitar el botón para evitar más clics
+                }
+            }
+        }
     }
 }
