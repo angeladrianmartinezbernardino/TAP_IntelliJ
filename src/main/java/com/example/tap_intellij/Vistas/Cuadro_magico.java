49a57db79d6390652d76a319bad7cffaad625fcc
Ángel Adrián Martínez Bernardino <angeladrianmartinezbernardino@gmail.com>
2024-03-03 20:54:09 -0600

    Ajustando detalles.

--- src/main/java/com/example/tap_intellij/Vistas/Cuadro_magico.java
+++ src/main/java/com/example/tap_intellij/Vistas/Cuadro_magico.java
@@ -1,13 +1,11 @@
 package com.example.tap_intellij.Vistas;
 
-import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
