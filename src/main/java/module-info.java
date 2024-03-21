90487a8bf50b056f2bfca2ca3e843e4b6ae19039
Ángel Adrián Martínez Bernardino <angeladrianmartinezbernardino@gmail.com>
2024-03-21 14:07:13 -0600

    Ajustando detalles.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,12 @@ module com.example.tap_intellij {
 
     opens com.example.tap_intellij to javafx.fxml;
     opens com.example.tap_intellij.Componentes to javafx.base;
+    opens com.example.tap_intellij.Modelos to javafx.base;
     exports com.example.tap_intellij;
 
-    requires org.kordamp.bootstrapfx.core;
     requires java.sql;
+    // requires mysql.connector.j;
     requires mariadb.java.client;
+
+    requires org.kordamp.bootstrapfx.core;
 }
